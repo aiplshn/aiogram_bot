aaeb223880465fb04df2a6f3298e03ac2d9a7fbf
Alexander Polushin <studmpeipolushin@gmail.com>
2022-09-21 21:45:26 +0300

    Close connection on shutdown

--- __main__.py
+++ __main__.py
@@ -1,5 +1,5 @@
 from aiogram.utils import executor
-from bot.create_bot import dp, BOT_DATA
+from bot.create_bot import dp, BOT_DATA, CONTROLLER_PRICE
 from bot.handlers import admin
 from aiogram.types import InlineKeyboardButton
 from bot.handlers.admin import admin
@@ -15,9 +15,14 @@ async def on_startup(_):
     for i in BOT_DATA.chat_ids:
         print(i)
         
+async def on_shutdown(_):
+    CONTROLLER_PRICE.close_all_connections()
+    print('Poka')
+
+
 admin.register_handler_admin(dp)
 client.register_handler_client(dp)
 other.register_handler_other(dp)
 
 
-executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
+executor.start_polling(dp, skip_updates=True, on_startup=on_startup, on_shutdown=on_shutdown)
